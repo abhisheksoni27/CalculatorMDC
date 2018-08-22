ebcf632f98914aba28e329c1d8b5989e36c3768b
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-08-22 15:46:44 +0530

    listeners for all buttons; numbers and operators

--- app/src/main/java/codeprose/calculatormdc/MainActivity.kt
+++ app/src/main/java/codeprose/calculatormdc/MainActivity.kt
@@ -2,27 +2,40 @@ package codeprose.calculatormdc
 
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.line_0.*
+import kotlinx.android.synthetic.main.line_123.*
+import kotlinx.android.synthetic.main.line_456.*
+import kotlinx.android.synthetic.main.line_789.*
+import kotlinx.android.synthetic.main.line_operators.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private var mInput: EditText? = null
+    private var buttonListener: View.OnClickListener = View.OnClickListener { view ->
 
-    private var divideButton: MaterialButton?= null
-    private var mulitplyButton: MaterialButton?= null
-    private var minusButton: MaterialButton?= null
-    private var plusButton: MaterialButton?= null
+        var temp = input?.text
 
-    private var buttonListener: View.OnClickListener = View.OnClickListener {
+        if (view is MaterialButton) {
+            input?.setText(temp.toString() + " " + view.text.toString())
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mInput = findViewById(R.id.input)
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        val buttonsArray = listOf(divide_button, multiply_button, minus_button, plus_button, one_button, two_button, three_button, four_button, five_button, six_button, seven_button, eight_button, nine_button, zero_button)
+
+        for (button in buttonsArray) {
+            button.setOnClickListener(buttonListener)
+        }
+
     }
 
 }
