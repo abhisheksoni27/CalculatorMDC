080b10986da97f7fb10cbaf4146f82d6a4b9ad3e
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-08-22 15:21:05 +0530

    add ids to each button

--- app/src/main/java/codeprose/calculatormdc/MainActivity.kt
+++ app/src/main/java/codeprose/calculatormdc/MainActivity.kt
@@ -1,15 +1,28 @@
 package codeprose.calculatormdc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val input: EditText? = null
+    private var mInput: EditText? = null
+
+    private var divideButton: MaterialButton?= null
+    private var mulitplyButton: MaterialButton?= null
+    private var minusButton: MaterialButton?= null
+    private var plusButton: MaterialButton?= null
+
+    private var buttonListener: View.OnClickListener = View.OnClickListener {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mInput = findViewById(R.id.input)
     }
+
 }
